63504b331d39d8a88ec143a16836634f30382069
Zhanysh <arcsinner@mail.ru>
2016-11-21 03:37:57 +0600

    Пару правок и добавление пару комментов

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
  * 1) Свой метод по пиксельно
  * 2) Готовой функцией OpenCV по умножению матриц. 1 изображение переведите сперва черно-белое 8-битное
  * */
-void my_roi(Mat img, Mat mask,Mat output){
+void my_roi(Mat img, Mat mask,Mat output){//мой метод
     for (int y = 0; y < img.rows; y++){
         for (int x = 0; x < img.cols; x++){
             if(mask.at<uchar>(y,x)==255){
@@ -29,7 +29,7 @@ int main() {
     Mat out(rows, cols, CV_8U, Scalar(0));
     Mat out2(rows, cols, CV_8U, Scalar(0));
 
-
+    /* Нарисовал белые круги на Mat mask*/
     circle(mask, Point(163,110),40, Scalar(255,255,255),CV_FILLED, 8,0);
     circle(mask, Point(260,100),45, Scalar(255,255,255),CV_FILLED, 8,0);
     circle(mask, Point(373,133),45, Scalar(255,255,255),CV_FILLED, 8,0);
@@ -37,8 +37,8 @@ int main() {
 
     img.copyTo(out, mask);//готовый метод
     my_roi(img,mask,out2);//мой метод
-    imshow("img",img);
-    imshow("img2",mask);
+    imshow("img",img);//изображение
+    imshow("MASK",mask);//маска
     imshow("out",out);
     imshow("out2",out2);
    // imshow("mask",mask);
